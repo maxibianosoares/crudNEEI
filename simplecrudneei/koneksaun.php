2c818579b04a6d1100eef17e0dce088d1817ea25
maxibianosoares <54614014+maxibianosoares@users.noreply.github.com>
2022-11-07 09:32:10 +0900

    koko
    koko

--- simplecrudneei/koneksaun.php
+++ simplecrudneei/koneksaun.php
@@ -4,6 +4,7 @@ $user = "root";	//username phpMyAdmin
 $pass = "";	//password phpMyAdmin
 $name = "crud_simples";	//naran database
 
+//koko
 $koneksaun = mysql_connect($host, $user, $pass) or die("Koneksaun ba bazedadus falla!");
 mysql_select_db($name, $koneksaun) or die("La iha baze dadus neebe mk eskolla!");
 ?>
